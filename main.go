d6c49c6547048554398db0e22ac8654b70f2b765
Danilo Fernando Deverso <danilo.deverso@dft-sp-wkn918.dafiti.local>
2019-12-10 17:19:39 -0300

    Teste heroku

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 )
 
-type Dog struct {
-	Breed    string
-	WeightKg int
+func main () {
+	fmt.Println("Testeeee")
 }
 
-func main() {
-	d := Dog{
-		Breed:    "dalmation",
-		WeightKg: 45,
-	}
-	b, _ := json.Marshal(d)
-	fmt.Println(string(b))
-}
+// type Dog struct {
+// 	Breed    string
+// 	WeightKg int
+// }
+
+// func main() {
+// 	d := Dog{
+// 		Breed:    "dalmation",
+// 		WeightKg: 45,
+// 	}
+// 	b, _ := json.Marshal(d)
+// 	fmt.Println(string(b))
+// }
